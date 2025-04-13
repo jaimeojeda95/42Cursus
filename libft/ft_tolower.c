ddecf7d58c388b1e996799987b531f4ee9a2b545
Jaime Leonardo Ojeda Vargas <jaojeda-@c2r6s4.42madrid.com>
2025-04-13 16:49:49 +0200

    ft_toupper.c y ft_tolower.c con norminette

--- libft/ft_tolower.c
+++ libft/ft_tolower.c
@@ -1,33 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jaojeda- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/13 15:23:01 by jaojeda-          #+#    #+#             */
-/*   Updated: 2025/04/13 16:49:12 by jaojeda-         ###   ########.fr       */
+/*   Created: 2025/04/13 16:39:41 by jaojeda-          #+#    #+#             */
+/*   Updated: 2025/04/13 16:48:49 by jaojeda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_toupper(int c)
+int	ft_tolower(int c)
 {
-	if (c >= 97 && c <= 122)
-		return (c - 32);
+	if (c >= 65 && c <= 90)
+		return (c + 32);
 	return (c);
 }
 /*
 int	main(void)
 {
-	char	mensaje[] = "hoLA empa$4nada de pollo";
+	char	mensaje[] = "HOLA Coddy$4";
 	int	i;
 
 	i = 0;
 	while (mensaje[i] != '\0')
 	{
-		mensaje[i] = ft_toupper(mensaje[i]);
+		mensaje[i] = ft_tolower(mensaje[i]);
 		i++;
 	}
 	printf("%s\n", mensaje);
