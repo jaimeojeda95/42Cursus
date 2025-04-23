b9a8f20e320c95422a41fa6ef9963477fd8c238c
Jaime Leonardo Ojeda Vargas <jaojeda-@c2r2s4.42madrid.com>
2025-04-24 00:05:34 +0200

    itoa.c

---
+++ libft/ft_itoa.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaojeda- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 22:56:11 by jaojeda-          #+#    #+#             */
+/*   Updated: 2025/04/24 00:04:43 by jaojeda-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static int	ft_contar_numeros(int n)
+{
+	int	num_total;
+	int	contador;
+	int	num;
+
+	contador = 1;
+	num_total = 1;
+	if (n < 0)
+	{
+		contador = -1;
+		n = -n;
+		num_total++;
+	}
+	else if (n == 0)
+		return (1);
+	num = n;
+	while ((n / 10) > 0)
+	{
+		n = n / 10;
+		num_total++;
+	}
+	return (num_total);
+}
+
+char	*ft_itoa(int n)
+{
+	int		num_total;
+	int		num_aux;
+	char	*str;
+
+	num_total = ft_contar_numeros(n);
+	num_aux = num_total;
+	str = malloc(num_total + 1);
+	if (str == NULL)
+		return (NULL);
+	if (n < 0)
+	{
+		n = -n;
+		str[0] = '-';
+	}
+	else if (n == 0)
+		str[num_total - 1] = ((n % 10) + '0');
+	while (n > 0)
+	{
+		str[num_total - 1] = ((n % 10) + '0');
+		n = (n / 10);
+		num_total--;
+	}
+	str[num_aux] = '\0';
+	return (str);
+}
+/*
+int	main(void)
+{
+	int	num = 0;
+
+	printf ("El int convertida a char es: %s\n", ft_itoa(num));
+	return (0);
+}*/
