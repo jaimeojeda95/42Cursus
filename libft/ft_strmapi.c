7c50b7ad2498cfaafd4bdcb9cdf673c3e95fb0b3
Jaime Leonardo Ojeda Vargas <jaojeda-@c2r13s4.42madrid.com>
2025-04-24 14:50:53 +0200

    ft_strmapi.c

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaojeda- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 13:03:57 by jaojeda-          #+#    #+#             */
+/*   Updated: 2025/04/24 14:39:43 by jaojeda-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	int		num_total;
+	char	*resultado;
+
+	if (s == NULL || f == NULL)
+		return (NULL);
+	i = 0;
+	num_total = ft_strlen(s);
+	resultado = malloc(num_total + 1);
+	if (resultado == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		resultado[i] = f(i, s[i]);
+		i++;
+	}
+	resultado[i] = '\0';
+	return (resultado);
+}
