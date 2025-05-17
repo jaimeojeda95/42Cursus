8204c86da2591f6c187410347f2ecae2f83aee3f
Jaime Leonardo Ojeda Vargas <jaojeda-@c2r11s3.42madrid.com>
2025-05-17 19:27:35 +0200

    get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jaojeda- <jaojeda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 13:44:45 by jaojeda-          #+#    #+#             */
-/*   Updated: 2025/05/17 19:08:29 by jaojeda-         ###   ########.fr       */
+/*   Updated: 2025/05/17 19:27:17 by jaojeda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 char *get_next_line(int fd)
 {
 	ssize_t	bytes_read;
-	char	buffer[BUFFER_SIZE];
+	char	buffer[BUFFER_SIZE + 1];
+	char	*acumulate_str;
 	
 	fd = open("prueba.txt", O_RDONLY);
 	if (fd == -1)
@@ -24,6 +25,7 @@ char *get_next_line(int fd)
 		return(1);
 	}
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	while (bytes_read != '\n')
 	if (bytes_read == -1)
 	{
 		perror("Error al leer el archivo");
