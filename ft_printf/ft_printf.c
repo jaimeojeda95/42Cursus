8a87a976d5bdeb41225a97bee761f3b306cb47c4
Jaime Leonardo Ojeda Vargas <jaojeda-@c2r11s2.42madrid.com>
2025-05-17 12:58:17 +0200

    actualización (1 función menos)

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,11 +6,37 @@
 /*   By: jaojeda- <jaojeda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 15:49:31 by jaojeda-          #+#    #+#             */
-/*   Updated: 2025/05/16 21:46:01 by jaojeda-         ###   ########.fr       */
+/*   Updated: 2025/05/17 12:36:13 by jaojeda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	ft_printf_conditions(char format, va_list args)
+{
+	int	count;
+
+	count = 0;
+	if (format == 'c')
+		count += ft_putchar(va_arg(args, int));
+	if (format == 's')
+		count += ft_putstr(va_arg(args, char *));
+	if (format == 'p')
+		count += ft_validate_ptr(va_arg(args, void *));
+	if (format == 'd')
+		count += ft_putnbr(va_arg(args, int));
+	if (format == 'i')
+		count += ft_putnbr(va_arg(args, int));
+	if (format == 'u')
+		count += ft_unsigned_write(va_arg(args, unsigned int));
+	if (format == 'x')
+		count += ft_hexa_min(va_arg(args, unsigned long));
+	if (format == 'X')
+		count += ft_hexa_max(va_arg(args, unsigned long));
+	if (format == '%')
+		count += write(1, "%", 1);
+	return (count);
+}
 
 int	ft_printf(char const *str, ...)
 {
@@ -35,11 +61,3 @@ int	ft_printf(char const *str, ...)
 	va_end(args);
 	return (count);
 }
-/* int main(void)
-{
-	char *str = "hola";
-
-	ft_printf("%s\n", str);
-	printf("%s\n", str);
-	return 0;
-} */
