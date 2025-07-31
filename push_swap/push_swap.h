725e38451301c4dd8af90e1ad93565351e173a1b
Jaime Leonardo Ojeda Vargas <jaojeda-@c1r12s6.42madrid.com>
2025-07-31 21:54:16 +0200

    arreglo

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jaojeda- <jaojeda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 21:03:11 by jaojeda-          #+#    #+#             */
-/*   Updated: 2025/07/31 16:27:04 by jaojeda-         ###   ########.fr       */
+/*   Updated: 2025/07/31 21:54:01 by jaojeda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 # include <unistd.h>
 # include <stdarg.h>
 # include <limits.h>
+
+//
 
 char	***evaluate_arguments(int argc, char **argv);
 int		validate_args(int argc, char ***args);
